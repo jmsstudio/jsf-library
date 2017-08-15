8c90aa804d8e2d9f10cbd0cfe7c34b1fabcffdd5
Jefferson Mariano de Souza <jefferson.souza@synchro.com.br>
2017-08-15 14:12:46 -0300

    Projeto jsf base para utilizar CDI

---
+++ src/main/java/br/com/jmsstudio/livraria/bean/AutorBean.java
@@ -0,0 +1,65 @@
+package br.com.jmsstudio.livraria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.jmsstudio.livraria.dao.DAO;
+import br.com.jmsstudio.livraria.modelo.Autor;
+
+
+@ManagedBean
+public class AutorBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Autor autor = new Autor();
+	
+	private Integer autorId;
+	
+	
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
+	public void carregarAutorPelaId() {
+		this.autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
+	}
+
+	public String gravar() {
+		System.out.println("Gravando autor " + this.autor.getNome());
+
+		if(this.autor.getId() == null) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		} else {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
+
+		this.autor = new Autor();
+
+		return "livro?faces-redirect=true";
+	}
+	
+	public void remover(Autor autor) {
+		System.out.println("Removendo autor " + autor.getNome());
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+}
